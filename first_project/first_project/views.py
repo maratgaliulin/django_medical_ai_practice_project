6e1b7b84a663a9d57d01cbc579d01faeb2365d2c
Marat Galiulin <egomarat@gmail.com>
2024-03-12 13:26:15 +1100

    first push

---
+++ first_project/first_project/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
